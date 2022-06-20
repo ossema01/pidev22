12e84fbcd2d9352dc5e6df0aeddeefc5b0a62453
oabdenadher <oabdenadher@vermeg.com>
2022-06-20 20:43:07 +0100

    initial commit

---
+++ src/test/java/tn/esprit/wellBeing/WellBeingApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.wellBeing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellBeingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
