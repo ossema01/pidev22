473920a34208f7bd0ba1811d5b9a3f764f941341
ndridi <ndridi@vermeg.com>
2022-07-05 06:16:07 +0100

    nd-occurence-module

---
+++ src/main/java/tn/esprit/wellbeing/modules/occurences/controller/InvitationRestController.java
@@ -0,0 +1,60 @@
+package tn.esprit.wellbeing.modules.occurences.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.wellbeing.modules.occurences.models.Invitation;
+import tn.esprit.wellbeing.modules.occurences.services.IInvitationService;
+
+//InvitationRestController
+	@RestController // = @Controller + @ResponseBody 
+	//@Scope("session") 
+	//singleton c'est par défaut 
+	//prototype 
+	//request
+public class InvitationRestController {
+		// Couplage Faible 
+		@Autowired 
+		IInvitationService invitationService; 
+		// URL : http://localhost:????/????/retrieve-all-Invitations
+		@GetMapping("/retrieve-all-Invitations")
+		public List<Invitation> retrieveAllInvitations() {
+			List<Invitation> list = invitationService.retrieveAllInvitations();
+			return list;
+		}
+	 
+		// http://localhost:????/timesheet-devops/retrieve-invitation/{invitation-id}
+		@GetMapping("/retrieve-invitation/{employee-id}")
+		public Invitation retrieveInvitation(@PathVariable("employee-id") String invitationId) {
+			return invitationService.retrieveInvitation(invitationId);
+		}
+
+		// Ajouter invitation : http://localhost:????/timesheet-devops/add-invitation 
+		@PostMapping("/add-invitation")
+		public Invitation addInvitation(@RequestBody Invitation inv) {
+			Invitation invitation = invitationService.createInvitation(inv); 
+			return invitation;
+		}
+
+		
+		// Supprimer invitation : 
+		// http://localhost:????/timesheet-devops/remove-invitation/{invitation-id}
+		@DeleteMapping("/remove-invitation/{invitation-id}") 
+		public void removeInvitation(@PathVariable("invitation-id") String invitationId) { 
+			invitationService.deleteInvitation(invitationId);
+		} 
+		// Modifier invitation 
+		// http://localhost:????/timesheet-devops/modify-invitation 
+		@PutMapping("/modify-invitation") 
+		public Invitation updateInvitation(@RequestBody Invitation invitation) {
+			return invitationService.updateInvitation(invitation);
+		}
+}
