309dcc145bd143eed8278ecd0a316bcfffdee2cd
oabdenadher <oabdenadher@TNLT1437.vermeg.com>
2022-07-26 22:44:11 +0100

    fix

--- src/main/java/tn/esprit/wellbeing/modules/forum/models/Post.java
+++ src/main/java/tn/esprit/wellbeing/modules/forum/models/Post.java
@@ -1,12 +1,10 @@
 package tn.esprit.wellbeing.modules.forum.models;
 
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 import tn.esprit.wellbeing.modules.forum.AbstractForumObject;
 
-@Entity
-@Table(name="post")
+@Entity(name = "post_")
 public class Post extends AbstractForumObject<String>{
 	
 }
--- src/main/java/tn/esprit/wellbeing/modules/forum/repository/PostRepository.java
+++ src/main/java/tn/esprit/wellbeing/modules/forum/repository/PostRepository.java
@@ -10,16 +10,16 @@ import tn.esprit.wellbeing.modules.forum.models.Post;
 @Repository
 public interface PostRepository extends CrudRepository<Post, Long>{
 
-	@Query("UPDATE post SET anonymous=1 WHERE id = :id ")
+	@Query("UPDATE post_ SET anonymous=1 WHERE id = :id ")
 	void unAnonymize(@Param("id") Long id);
 
-	@Query("UPDATE post  SET anonymous=0 WHERE id = :id ")
+	@Query("UPDATE post_  SET anonymous=0 WHERE id = :id ")
 	void anonymize(@Param("id") Long id);
 
-	@Query("UPDATE post SET suspendedComments=0 WHERE id = :id ")
+	@Query("UPDATE post_ SET suspendedComments=0 WHERE id = :id ")
 	void activateComments(@Param("id") Long forumObjctId);
 
-	@Query("UPDATE post  SET suspendedComments=1 WHERE id = :id ")
+	@Query("UPDATE post_  SET suspendedComments=1 WHERE id = :id ")
 	void suspendComments(@Param("id") Long forumObjctId);
 	
 }
