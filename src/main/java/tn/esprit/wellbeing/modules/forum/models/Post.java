5cb53edd153eaa6e7def07ec402cb8f5299c0329
oabdenadher <oabdenadher@TNLT1437.vermeg.com>
2022-07-26 21:10:52 +0100

    fix sql

--- src/main/java/tn/esprit/wellbeing/modules/forum/models/Post.java
+++ src/main/java/tn/esprit/wellbeing/modules/forum/models/Post.java
@@ -1,10 +1,12 @@
 package tn.esprit.wellbeing.modules.forum.models;
 
 import javax.persistence.Entity;
+import javax.persistence.Table;
 
 import tn.esprit.wellbeing.modules.forum.AbstractForumObject;
 
 @Entity
+@Table(name="post")
 public class Post extends AbstractForumObject<String>{
 	
 }
