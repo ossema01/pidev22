186e573c0f657c355e5cf154050f295283405bad
oabdenadher <oabdenadher@TNLT1437.vermeg.com>
2022-07-26 21:08:14 +0100

    fix sql

--- src/main/java/tn/esprit/wellbeing/modules/forum/repository/RecommendationReposiory.java
+++ src/main/java/tn/esprit/wellbeing/modules/forum/repository/RecommendationReposiory.java
@@ -10,15 +10,15 @@ import tn.esprit.wellbeing.modules.forum.models.Recommendation;
 @Repository
 public interface RecommendationReposiory extends CrudRepository<Recommendation, Long> {
 
-	@Query("UPDATE Recommendation p SET p.anonymous=1 WHERE p.id = :id ")
+	@Query("UPDATE recommendation p SET p.anonymous=1 WHERE p.id = :id ")
 	void unAnonymize(@Param("id") Long id);
 
-	@Query("UPDATE Recommendation p SET p.anonymous=0 WHERE p.id = :id ")
+	@Query("UPDATE recommendation p SET p.anonymous=0 WHERE p.id = :id ")
 	void anonymize(@Param("id") Long id);
 
-	@Query("UPDATE Recommendation p SET p.suspendedComments=0 WHERE p.id = :id ")
+	@Query("UPDATE recommendation p SET p.suspendedComments=0 WHERE p.id = :id ")
 	void activateComments(@Param("id") Long forumObjctId);
 
-	@Query("UPDATE Recommendation p SET p.suspendedComments=1 WHERE p.id = :id ")
+	@Query("UPDATE recommendation p SET p.suspendedComments=1 WHERE p.id = :id ")
 	void suspendComments(@Param("id") Long forumObjctId);
 }
